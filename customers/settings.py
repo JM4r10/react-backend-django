64568880895d39a2774c62c56da9471f22dd8a31
JM4r10 <oiram1585@hotmail.com>
2023-02-22 17:12:30 -0500

    updated settings and requirements

--- customers/settings.py
+++ customers/settings.py
@@ -54,10 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
-]
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'customers.urls'
 
