0e34de68351da238d5fe7aa64a22826e75f2adda
JM4r10 <oiram1585@hotmail.com>
2023-02-27 17:57:51 -0500

    jwt - authentication apis with token

--- customers/views.py
+++ customers/views.py
@@ -1,13 +1,15 @@
 from customers.models import Customer
 from django.http import JsonResponse, Http404
 from customers.serializer import CustomerSerializer
-from rest_framework.decorators import api_view  # says which methods are allowed
+from rest_framework.decorators import api_view, permission_classes  # says which methods are allowed
 # use it for all responses: JSON response, 404, etc
 from rest_framework.response import Response
 from rest_framework import status  # options for status codes
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def customers(request):
     # invokes serializer and return to client
     if request.method == 'GET':
@@ -23,6 +25,7 @@ def customers(request):
 
 
 @api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def customer(request, id):
     try:
         data_db = Customer.objects.get(pk=id)
@@ -30,6 +33,7 @@ def customer(request, id):
         return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
+        # raise Exception()
         serializer = CustomerSerializer(data_db)
         return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
     elif request.method == 'DELETE':
